4823388dc7279444ebdf6ad2b535892205918889
AnonyMouse-Box <36849880+AnonyMouse-Box@users.noreply.github.com>
2024-08-25 17:57:38 +0100

    Ammended the webpage to allow different types of file to work.

--- src/web_page.py
+++ src/web_page.py
@@ -3,6 +3,7 @@
 
 # web_page.py
 
+import os
 from functools import cached_property
 from http.cookies import SimpleCookie
 from http.server import BaseHTTPRequestHandler
@@ -31,18 +32,33 @@ class WebRequestHandler(BaseHTTPRequestHandler):
         return SimpleCookie(self.headers.get("Cookie"))
 
     def do_GET(self):
-        self.send_response(200)
-        self.send_header("Content-Type", "text/html")
-        self.end_headers()
-        self.wfile.write(self.get_response())
+        try:
+            self.send_response(200)
+            if self.path == "/":
+                self.path = "/web/"
+            if self.path.endswith("/"):
+                self.path += "index.html"
+            if (self.path.endswith(".html")):
+                self.send_header("Content-type", "text/html")
+            elif (self.path.endswith(".css")):
+                self.send_header("Content-type", "text/css")
+            elif self.path.endswith("favicon.ico"):
+                self.send_header('Content-Type', 'image/x-icon')
+                self.send_header('Content-Length', 0)
+                self.end_headers()
+                return
+            self.end_headers()
+            self.wfile.write(bytes(self.get_response(os.path.join('.', self.path[1:]))))
+        except IOError:
+            self.send_error(404, "File Not Found: %s" % self.path)
 
     def do_POST(self):
         self.do_GET()
 
-    def get_response(self):
+    def get_response(self, file):
         data = b""
-        with open('web/index.html', 'rb') as file_in:
-            data += file_in.read()
+        with open(file, 'rb') as file:
+            data += file.read()
         return data
 
 # Throw an error if run directly.
